b5cacc8ad5b6b0d2709c7b1886716f8ee1d5773e
Deepu Raj <itsdeepuraj@gmail.com>
2021-05-08 19:17:17 +0530

    Update FindImmediateGreaterThanX.py

--- FindImmediateGreaterThanX.py
+++ FindImmediateGreaterThanX.py
@@ -1,23 +1,28 @@
-def getByIndex(arr,n,idx):
-    if idx>=n:
-        return -1
-    else:
-        return arr[idx]
-     
+class Solution:
+    def greaterThanX(self,arr,n,x):
+        #return required result
+        #code here
+        count=0
+        res = []
+        for e in arr:
+            if e > x:
+                res.append(e)
+                count+=1
+                
+        return count
 
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
 
-#contributed by RavinderSinghPB
-if __name__ == '__main__':
+if __name__ =='__main__':
     tcs=int(input())
     
     for _ in range(tcs):
         n=int(input())
-        arr=[int(x) for x in input().split()]
-        
-        idx=int(input())
-        
-        print(getByIndex(arr,n,idx))
+        arr=[int(e) for e in input().split()]
+        x=int(input())
+        ob=Solution()
+        ans=ob.greaterThanX(arr,n,x)
+        print(ans)
 # } Driver Code Ends
