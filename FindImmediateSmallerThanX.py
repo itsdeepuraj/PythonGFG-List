4490851f2b46c3d47f15af727c92804da5222f81
Deepu Raj <itsdeepuraj@gmail.com>
2021-05-08 19:11:39 +0530

    Update FindImmediateSmallerThanX.py

--- FindImmediateSmallerThanX.py
+++ FindImmediateSmallerThanX.py
@@ -1,23 +1,31 @@
-def getByIndex(arr,n,idx):
-    if idx>=n:
-        return -1
-    else:
-        return arr[idx]
-     
+class Solution:
+    # inf has been imported in driver code
+    def immediateGreater(self,arr,n,x):
+        #return required ans
+        
+        #code here
+        ans=-1
+        diff = 10000000
+        for e in arr:
+            if e>x and e-x < diff:
+                ans=e
+                diff = e-x
+        return ans
 
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
 
 #contributed by RavinderSinghPB
-if __name__ == '__main__':
+if __name__ =='__main__':
+    from math import inf
     tcs=int(input())
     
     for _ in range(tcs):
         n=int(input())
-        arr=[int(x) for x in input().split()]
+        arr=[int(e) for e in input().split()]
+        x=int(input())
         
-        idx=int(input())
-        
-        print(getByIndex(arr,n,idx))
+        ans=Solution().immediateGreater(arr,n,x)
+        print(ans)
 # } Driver Code Ends
